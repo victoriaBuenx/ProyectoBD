b1b07dd11b1b8b8b43a566b930db8836eef7061e
victoriaBuenx <victoriabuenomijares@gmail.com>
2025-04-16 00:08:47 -0600

    Test modulo de reportes
    Pruebas con reportes

---
+++ Clinica/src/clinica/ReporteClinica.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clinica;
+
+import java.util.HashMap;
+import java.sql.Connection;
+import java.util.Map;
+import javax.swing.WindowConstants;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.view.JasperViewer;
+import net.sf.jasperreports.engine.util.JRLoader;
+
+/**
+ *
+ * @author victo
+ */
+public class ReporteClinica {
+
+    public void generarReporte() {
+        try {
+            JasperReport report = (JasperReport) JRLoader.loadObject(
+                getClass().getResource("/reportes/ClinicaDental.jasper")
+            );
+
+            Conexion conexionBD = new Conexion();
+            Connection con = conexionBD.conexion(); 
+
+            if (con != null) {
+                JasperPrint jprint = JasperFillManager.fillReport(report, null, con);
+
+                JasperViewer view = new JasperViewer(jprint, false);
+                view.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                view.setVisible(true);
+            } else {
+                System.out.println("No se pudo establecer conexión con la base de datos.");
+            }
+
+        } catch (JRException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+    
